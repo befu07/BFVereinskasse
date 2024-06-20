ec02f353ff01b62ae72d6434d4f1576e4c7f7470
Bernhard Futtera <ITN276497@qualifizierung.at>
2024-06-20 09:37:47 +0200

    Projektdateien hinzufügen.

---
+++ BFVereinskasse/_database.sql
@@ -0,0 +1,25 @@
+Create Database BFVereinskasse
+go
+
+ALTER DATABASE BFVereinskasse SET SINGLE_USER WITH ROLLBACK IMMEDIATE 
+GO
+Drop DATABASE if exists BFVereinskasse
+GO
+
+use BFVereinskasse
+go
+
+Create Table Vereinsmitglied(
+	Id int Identity not null, 
+	Vorname varchar(255),
+	Nachname varchar(255),
+	IsActive bit,
+	Bild image
+)
+Create Table Zahlungen(
+	Id int Identity not null, 
+	VereinsmitgliedId int not null,
+	Betrag money not null,
+	Datum datetime not null, 
+	Beschreibung varchar(50),
+)
