a3268ea34e1a06baac86d9899c1bfabdc12e3516
Bernhard Futtera <ITN276497@qualifizierung.at>
2024-06-20 13:08:54 +0200

    createbaustelle

--- BFVereinskasse/Controllers/PaymentController.cs
+++ BFVereinskasse/Controllers/PaymentController.cs
@@ -1,4 +1,5 @@
-﻿using BFVereinskasse.Services;
+﻿using BFVereinskasse.Models;
+using BFVereinskasse.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BFVereinskasse.Controllers
@@ -14,10 +15,36 @@ namespace BFVereinskasse.Controllers
         }
 
         [HttpGet]
-        public IActionResult New()
+        public async Task<IActionResult> CreatePayment()
         {
+            var vm = new CreatePaymentVM();
+            vm.Members = await _memberService.GetMembers();
+            return View(vm);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreatePayment(CreatePaymentVM form)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", controllerName: "Home");
+            }
+            else
+            {
+                form.Members = await _memberService.GetMembers();
+                var errormessages = ModelState.Values.SelectMany(v => v.Errors.Select(b => b.ErrorMessage));
+                var errorstring = string.Join(", ", errormessages);
+                TempData["ErrorMessage"] = errorstring;
+                return View(form);
 
-            return View();
+
+                //var vm = new CreatePaymentVM();
+                //vm.Members = await _memberService.GetMembers();
+                //vm.DateTimePickerValue = form.TimeStamp.ToString("yyyy-MM-ddTHH:mm:ss");
+                //vm.Description = form.Description;
+                //vm.MemberId = form.MemberId;
+                //vm.Amount = form.Amount;
+                //return RedirectToAction(nameof(CreatePayment));
+            }
         }
     }
 }
---
+++ BFVereinskasse/Models/CreatePaymentVM.cs
@@ -0,0 +1,59 @@
+﻿using BFVereinskasse.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace BFVereinskasse.Models
+{
+    public class CreatePaymentVM
+    {
+        public List<Mitglied>? Members { get; set; }
+
+        [Required]
+        [Display(Name = "Mitglied")]
+        public int? MemberId { get; set; }
+
+        [Display(Name = "Beschreibung")]
+        [MaxLength(50)]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Pflichtfeld: Betrag")]
+        [Display(Name = "Betrag")]
+        //[DataType(DataType.Currency)] // geht ned
+        public decimal? Amount { get; set; }
+        [PastDate]
+        [Display(Name = "Datum")]
+        public DateTime TimeStamp { get; set; }
+        //DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss").Replace(' ', 'T');
+        public string DateTimePickerValue { get; set; } = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+
+    }
+
+
+
+    public class PastDateAttribute : ValidationAttribute
+    {
+        public PastDateAttribute()
+        {
+            const string defaultErrorMessage = "geht ned";
+            ErrorMessage ??= defaultErrorMessage;
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                //return ValidationResult.Success;
+                return new ValidationResult("Required");
+            }
+            if (!DateTime.TryParse(value.ToString(), out DateTime date))
+            {
+                return new ValidationResult(
+                    FormatErrorMessage(validationContext.DisplayName));
+            }
+            if (date > DateTime.Now)
+            {
+                return new ValidationResult("Date must be now or in the past");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
